bb3644a758eb50779366abe95f23ba6f9fe15ed7
ivan-debug867 <gregoriustriwibowo@gmail.com>
2026-08-13 20:30:16 +0700

    push pull skip

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -1859,7 +1859,7 @@ class DbHelper {
 
     final response = await _client
         .from('forum_bookmarks')
-        .select(', forum_posts(, users(*))')
+        .select('*, forum_posts!inner(*, users(*))')
         .eq('user_id', user.id)
         .order('created_at', ascending: false);
 
